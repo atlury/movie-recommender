1928cd09dda5e293c971a3d96c722ae5b26d6ac8
Kamesh Kotwani <kameshkotwani@outlook.com>
2022-05-19 21:02:15 +0530

    updated with better exception handling and shared on streamlit now

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 import requests
 import streamlit as st
-import pickle
+import joblib
 import os
-API_KEY=os.getenv('SECRET_KEY')
+
+API_KEY = st.secrets['API_KEY']
 IMAGE_FULL_PATH = "https://image.tmdb.org/t/p/w500"
+
 def fetch_poster(movie_id):
     response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}')
     data = response.json()
     return IMAGE_FULL_PATH+data['poster_path']
 
-movies_df = pickle.load(open('models/movies.pkl','rb'))
-similarity = pickle.load(open('models/similarity.pkl','rb'))
-movies_titles = movies_df['title'].values
+try:
+    # getting the models
+    movies_df = joblib.load('models/movies.joblib')
+    similarity = joblib.load('models/similarity.joblib')
+    movies_titles:list=list(movies_df['title'].values)
+except Exception as e:
+    st.error("something went wrong from our end.")
+
+# getting the movie titles
+movies_titles.insert(0,None)
 
 def recommend(movie):
     movie_index = movies_df[movies_df['title']==movie].index[0]
@@ -25,30 +34,36 @@ def recommend(movie):
         posters.append(fetch_poster(movie_id)) 
     return recommended_movies,posters
 
-st.title('Basic Movie Recommender System')
+st.title('Movie Recommender System')
 st.subheader("Recommends top 5 movies of similar content across 5000 movies")
 
-option = st.selectbox('Movies',movies_titles)
+option = st.selectbox('Movies',options=(movies_titles))
 
-if st.button('Recommend',key=option):
-    if option is not None:
-        names,posters = recommend(option)
-        col1, col2, col3,col4,col5 = st.columns(5)
-        with col1:
-            st.text(names[0])
-            st.image(posters[0])
+try:
+    with st.spinner("## Fetching awesome movies!!"):
+        if option is not None:
+                names,posters = recommend(option)
+                col1, col2, col3,col4,col5 = st.columns(5)
+                
+                with col1:
+                    st.text(names[0])
+                    st.image(posters[0])
+                with col2:
+                    st.text(names[1])
+                    st.image(posters[1])
 
-        with col2:
-            st.text(names[1])
-            st.image(posters[1])
+                with col3:
+                    st.text(names[2])
+                    st.image(posters[2])
 
-        with col3:
-            st.text(names[2])
-            st.image(posters[2])
-
-        with col4:
-            st.text(names[3])
-            st.image(posters[3])
-        with col5:
-            st.text(names[4])
-            st.image(posters[4])
+                with col4:
+                    st.text(names[3])
+                    st.image(posters[3])
+                with col5:
+                    st.text(names[4])
+                    st.image(posters[4])
+        else:
+            st.title("Go ahead, try to search some movie!")
+            st.write("### Visit my [GitHub](https://www.github.com/kameshkotwani) page for more awesome projects!")
+except Exception as e:
+    st.info("Perhaps, my database is too small to recommend movies similar to your choice, maybe try some other movie :) ")
